0515914be4d925a5c045a415190d476a1171f331
VamsiM-Coder <mandalavamsi372@gmail.com>
2025-05-08 19:35:31 +0530

    Hibernate proj 1st commit

---
+++ src/main/java/org/myhib/Main.java
@@ -0,0 +1,28 @@
+package org.myhib;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws Exception {
+         Studentt student1 = new Studentt();
+         student1.setId(38);
+         student1.setName("Mahesh");
+         student1.setAge(23);
+
+         Configuration con = new Configuration();
+         con.addAnnotatedClass(Studentt.class);
+         con.configure("hibernate.cfg.xml");
+         SessionFactory sf = con.buildSessionFactory();           // It is a heavy weight object it is only used once
+         Session session = sf.openSession();
+         session.save(student1);
+
+         Transaction txn = session.beginTransaction();
+         txn.commit();
+         System.out.println(student1);
+    }
+}
---
+++ src/main/java/org/myhib/Studentt.java
@@ -0,0 +1,45 @@
+package org.myhib;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Studentt {
+    @Id
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
