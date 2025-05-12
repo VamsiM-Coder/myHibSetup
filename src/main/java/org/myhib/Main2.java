e35b8ba353a78327af83e5c450e5ea0098508c26
VamsiM-Coder <mandalavamsi372@gmail.com>
2025-05-12 19:54:34 +0530

    My mapping 2 nd  commit

--- src/main/java/org/myhib/Main2.java
+++ src/main/java/org/myhib/Main2.java
@@ -5,11 +5,13 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import java.util.Arrays;
+
 public class Main2 {
     public static void main(String[] args) {
 
         Address add1 = new Address();
-        add1.setLid(1);
+        add1.setLid(7);
         add1.setStreet("Bairagapateeda");
         add1.setCity("Tirupati");
         add1.setPincode(517501);
@@ -17,27 +19,30 @@ public class Main2 {
 
 
         Address add2 = new Address();
-        add2.setLid(2);
+        add2.setLid(8);
         add2.setStreet("Madiwala");
         add2.setCity("Banglore");
         add2.setPincode(560068);
         add2.setState("Karnataka");
 
 
-        //one2one
+//        one2one
 //       Jashwanthuuu thuu1 = new Jashwanthuuu();
 //       thuu1.setEmp_id(101);
 //       thuu1.setEmp_name("Dedee");
 //       thuu1.setEmp_phone("9915331135");
 //       thuu1.setA1(add);
-
         //one2Many
         Many2Many many1 = new Many2Many();
         many1.setId(102);
         many1.setEmp_name("Jashwanth Kumar");
         many1.setEmp_phone("8688751909");
+        many1.setAddress(Arrays.asList(add1,add2));
+
+
         SessionFactory sf = new Configuration()
-                .addAnnotatedClass(Jashwanthuuu.class)
+                //.addAnnotatedClass(Jashwanthuuu.class)
+                .addAnnotatedClass(Many2Many.class)
                 .addAnnotatedClass(Address.class)
                 .configure().
                 buildSessionFactory();
@@ -45,15 +50,16 @@ public class Main2 {
         Session session = sf.openSession();
 
         Transaction txn = session.beginTransaction();
-        session.persist(add);
-        session.persist(thuu1);
+        session.persist(add1);
+        session.persist(add2);
+        session.persist(many1);
         txn.commit();
 
         // After embedded the some fields to data to print this
-        Jashwanthuuu j1 = session.get(Jashwanthuuu.class, 1);
+        //Jashwanthuuu j1 = session.get(Jashwanthuuu.class, 1);
         // We are retriving the info but hibernate write insert query inspite of select what is the problem ???
         //Next Class
-        System.out.println(j1);
+        //System.out.println(j1);
 
         session.close();
         sf.close();
