3c499c24ab48e632d3e821f9665cead92246a509
VamsiM-Coder <mandalavamsi372@gmail.com>
2025-06-11 21:37:36 +0530

    added retrieve many info

---
+++ src/main/java/org/retrieve/many/Main.java
@@ -0,0 +1,25 @@
+package org.retrieve.many;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.insert.Student;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> s1 = null;
+
+        SessionFactory sf = new Configuration()
+                .addAnnotatedClass(Student.class)
+                .configure()
+                .buildSessionFactory();
+        Session s = sf.openSession();
+
+        s1 = s.createQuery("from Student", Student.class).list();
+
+        s.close();
+        sf.close();
+        System.out.println(s1);
+    }
+}
