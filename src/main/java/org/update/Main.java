23e5c8d32495d4957310bcb79782602949c740d8
VamsiM-Coder <mandalavamsi372@gmail.com>
2025-06-14 19:29:28 +0530

    added update query

--- src/main/java/org/update/Main.java
+++ src/main/java/org/update/Main.java
@@ -10,7 +10,15 @@ import org.insert.Student;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setId(5);
+        s1.setName("Vamsi");
+        s1.setAge(21);
 
+        Student s2 = new Student();
+        s1.setId(4);
+        s1.setName("Dedee");
+        s1.setAge(22);
 
         SessionFactory sf = new Configuration()
                 .addAnnotatedClass(Student.class)
@@ -20,28 +28,15 @@ public class Main {
         Session session = sf.openSession();
 
 
+        Transaction txn = session.beginTransaction();
 
-        //While fetching the data transaction is not important, it is used when you go for manipulation
-        // Transaction txn = session.beginTransaction();
-        Student student2 = session.get(Student.class,46);
-
-        //Update
-        //Transaction txn = session.beginTransaction();
-
-        // This query first fetch the particular id if it is not found it creates a row otherwise it give updated result
-        //session.merge(student1);
-
-
-
-        // delete
-        // FIRST we want to get the data by id and it fetch id details and wrote a delete query
-//        student1 = session.get(Student.class,89);
-//        Transaction txn = session.beginTransaction();
-//
-//        session.remove(student1);
-//        txn.commit();
-//        session.close();
-//        sf.close();
-//        System.out.println(student2);
+        // This query first fetch the particular id if it is not found it creates a new row with 0 id otherwise it give updated result
+        session.merge(s1);
+       // session.merge(s2);
+        txn.commit();
+        session.close();
+        sf.close();
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
